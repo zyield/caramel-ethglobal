7eadfda9bb702f7cb28638323933bada953c0553
Razvan Draghici <tzumby@gmail.com>
2022-09-14 21:28:38 -0400

    Styles

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="bg-white w-full text-center mt-20">
+      <div className="py-12">
+        <div className="mt-8">
+          <p className="text-center text-base text-gray-400">Coming soon - Own your content, migrate your posts from Medium + other platforms</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,14 @@ export default function Hero() {
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="text-center">
           <h2 className="text-4xl font-semibold text-indigo-600 tracking-wide uppercase">
-            crml.eth
+            Caramel
           </h2>
           <p className="max-w-xl mt-2 mx-auto text-lg text-gray-500">
-            Decentralized forever blog
+            Launch a decentralized, censorship-resistant blog on your .Eth domain
+          </p>
+
+          <p className="mt-8 text-gray-500">
+            Don’t have a .Eth domain yet? Get it <a className="text-indigo-500 hover:text-indigo-600 underline" href="https://app.ens.domains/" target="_blank">here</a>!
           </p>
         </div>
       </div>
