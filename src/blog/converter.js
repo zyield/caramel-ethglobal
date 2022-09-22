02e3409c446d521cdf4ae93dcfa4e4d5201ad7d7
Razvan Draghici <tzumby@gmail.com>
2022-09-22 08:07:11 -0400

    Update showdown rendering options

--- src/blog/converter.js
+++ src/blog/converter.js
@@ -1,5 +1,23 @@
 import showdown from 'showdown'
 
-let converter = new showdown.Converter()
+let opts = {
+  'omitExtraWLInCodeBlocks': true,
+  'noHeaderId': false,
+  'parseImgDimensions': true,
+  'simplifiedAutoLink': true,
+  'literalMidWordUnderscores': true,
+  'strikethrough': true,
+  'tables': true,
+  'tablesHeaderId': false,
+  'ghCodeBlocks': true,
+  'tasklists': true,
+  'smoothLivePreview': true,
+  'prefixHeaderId': false,
+  'disableForced4SpacesIndentedSublists': false,
+  'ghCompatibleHeaderId': true,
+  'smartIndentationFix': false
+}
+
+let converter = new showdown.Converter(opts)
 
 export const convert = markdown => converter.makeHtml(markdown)
