93777763ff120f48e1c76b9318343f09e0c19972
Razvan Draghici <tzumby@gmail.com>
2022-09-26 07:09:29 -0400

    Adds discord link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import discord_logo_svg from '../images/discord.svg'
+
 function Footer() {
   return (
     <footer className="w-full text-center mt-32 border-zinc-100 pt-10 pb-16 border-t dark:border-zinc-700/40">
@@ -5,6 +7,9 @@ function Footer() {
         Coming soon - Own your content, migrate your posts from Medium + other
         platforms
       </p>
+      <div className="mt-8 flex justify-center">
+        <a href="https://discord.gg/rBmyfNAP" target="_blank"><img src={discord_logo_svg} className="w-10" /></a>
+      </div>
     </footer>
   )
 }
